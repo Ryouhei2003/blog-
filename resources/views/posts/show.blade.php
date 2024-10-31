da29a445bd649b653043bc9e025229434064e1bb
Ryohei2003 <r.toriko9090@icloud.com>
2024-11-01 06:06:35 +0900

    Your commit message describing the changes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ブログ投稿詳細</title>
-</head>
-<body>
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
-    <a href="{{ url('/posts') }}">ブログ投稿一覧へ戻る</a>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Posts</title>
+    </head>
+    <body>
+        <h1>{{ $post->title }}</h1> <!-- 投稿タイトルを表示 -->
+    <p>{{ $post->body }}</p> <!-- 投稿本文を表示 -->
+    <a href="{{ url('/posts') }}">ブログ投稿一覧へ戻る</a> <!-- 投稿一覧ページへのリンク -->
+    <div class="edit">
+        <a href="/posts/{{ $post->id }}/edit">edit</a> <!-- 編集ページへのリンク -->
+    </div>
 </body>
 </html>
