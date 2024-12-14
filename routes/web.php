3ddaf4817e529e551a8f2e8846925204dc3fb959
Ryohei2003 <r.toriko9090@icloud.com>
2024-12-14 19:24:04 +0900

    完成

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\likeController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\VisitController;
 use App\Http\Controllers\CommentController;
 
